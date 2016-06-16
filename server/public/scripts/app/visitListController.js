eafe50883a9f1d2cb0cc61d9c583210f9cfe5c05
Paul D'Ambra <paul.dambra@gmail.com>
2016-06-16 18:42:24 +0100

    fix the broken test and allow visitlistcontroller to be able to toggle visited on a visit

--- server/public/scripts/app/visitListController.js
+++ server/public/scripts/app/visitListController.js
@@ -4,4 +4,9 @@ module.exports = function($scope, visitApi) {
   visitApi.getAll().then(function(res) {
     $scope.visits = res.data;
   });
+
+  $scope.toggleVisited = function(visit) {
+    var currentState = visit.visited || false;
+    visit.visited = !visit.visited;
+  };
 };
--- test/karma/list-controller.spec.js
+++ test/karma/list-controller.spec.js
@@ -4,6 +4,7 @@ require('../../server/public/scripts/app/main');
 
 describe('Listing visits', function() {
     var scope, visitListController;
+    var cities = [{}, {}];
 
     beforeEach(angular.mock.module('visitPlannr'));
 
@@ -15,13 +16,30 @@ describe('Listing visits', function() {
                 '$scope': scope,
                 'visitApi': {
                   getAll: function() {
-                    return [{}, {}, {}, {}];
+                    return {
+                      then: function(cb) {
+                        cb({data: cities});
+                      }
+                    };
                   }
                 }
             });
     }));
 
     it('should have a list of visits', function() {
-        expect(scope.visits.length).toBe(4);
+        expect(scope.visits.length).toBe(2);
+    });
+
+    it('should be able to toggle a city to visited state', function() {
+      expect(cities[1].visited).toBeFalsy();
+      scope.toggleVisited(cities[1]);
+      expect(cities[1].visited).toBe(true);
+    });
+
+    it('should be able to toggle a city to unvisited state', function() {
+      cities[0].visited = true;
+      expect(cities[0].visited).toBe(true);
+      scope.toggleVisited(cities[0]);
+      expect(cities[0].visited).toBe(false);
     });
 });
