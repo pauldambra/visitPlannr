d863a787e670e2146b3e097d3ea1062969671003
Paul D'Ambra <paul.dambra@gmail.com>
2016-06-16 16:01:10 +0100

    prove can bind visits to page

--- server/public/scripts/app/visitListController.js
+++ server/public/scripts/app/visitListController.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function($scope, visitApi) {
-  $scope.visits = visitApi.getAll();
+  visitApi.getAll().then(function(res) {
+    $scope.visits = res.data;
+  });
 };
